0518d1f7687d1280d5f4e1344da505ffefe9f111
Nikhil Anand <mail@nikhil.io>
2016-05-28 17:49:12 -0500

    Add Hamming Distance problem

---
+++ 007.hamming-distance/hamming.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def compute_hamming_distance(sequences):
+    '''Computes the Hamming Distance of an array of two sequences
+    of the same length.
+    '''
+
+    if len(sequences[0]) != len(sequences[1]):
+        raise ValueError('Sequences must be of the same length')
+
+    return len(
+        [
+            (sequences[0][_], sequences[1][_])
+            for _
+            in range(len(sequences[0]))
+            if sequences[0][_] != sequences[1][_]
+        ]
+    )
+
+
+if __name__ == '__main__':
+    print(
+        compute_hamming_distance(
+            open(sys.argv[1]).read().split()
+        )
+    )
