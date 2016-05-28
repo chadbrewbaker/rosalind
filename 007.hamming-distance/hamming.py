caa8d0a523868542a9505a5d99e62f6701ee16be
Nikhil Anand <mail@nikhil.io>
2016-05-28 17:58:42 -0500

    Update with better loop

--- 007.hamming-distance/hamming.py
+++ 007.hamming-distance/hamming.py
@@ -4,21 +4,28 @@ import sys
 def compute_hamming_distance(sequences):
     '''Computes the Hamming Distance of an array of two sequences
     of the same length.
-
-    BETTER ANSWER: Use zip() and count() instead...
     '''
 
     if len(sequences[0]) != len(sequences[1]):
         raise ValueError('Sequences must be of the same length')
 
-    return len(
-        [
-            (sequences[0][_], sequences[1][_])
-            for _
-            in range(len(sequences[0]))
-            if sequences[0][_] != sequences[1][_]
-        ]
-    )
+    # Will generate a huge list of tuples for large sequences...
+    # return len(
+    #     [
+    #         (sequences[0][_], sequences[1][_])
+    #         for _
+    #         in range(len(sequences[0]))
+    #         if sequences[0][_] != sequences[1][_]
+    #     ]
+    # )
+
+    hamming_distance = 0
+
+    for _ in range(len(sequences[0])):
+        if sequences[0][_] != sequences[1][_]:
+            hamming_distance += 1
+
+    return hamming_distance
 
 
 if __name__ == '__main__':
