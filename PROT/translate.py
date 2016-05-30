66fe02ee458e11f5cec4a40b4dad7962221d2309
Nikhil Anand <mail@nikhil.io>
2016-05-29 22:27:21 -0500

    Add translation

---
+++ PROT/translate.py
@@ -0,0 +1,90 @@
+'''Unnecessarily long list comprehension... a simple loop would've done
+the trick.
+'''
+
+import sys
+
+codon_table = {
+    'AAA': 'K',
+    'AAC': 'N',
+    'AAG': 'K',
+    'AAU': 'N',
+    'ACA': 'T',
+    'ACC': 'T',
+    'ACG': 'T',
+    'ACU': 'T',
+    'AGA': 'R',
+    'AGC': 'S',
+    'AGG': 'R',
+    'AGU': 'S',
+    'AUA': 'I',
+    'AUC': 'I',
+    'AUG': 'M',
+    'AUU': 'I',
+    'CAA': 'Q',
+    'CAC': 'H',
+    'CAG': 'Q',
+    'CAU': 'H',
+    'CCA': 'P',
+    'CCC': 'P',
+    'CCG': 'P',
+    'CCU': 'P',
+    'CGA': 'R',
+    'CGC': 'R',
+    'CGG': 'R',
+    'CGU': 'R',
+    'CUA': 'L',
+    'CUC': 'L',
+    'CUG': 'L',
+    'CUU': 'L',
+    'GAA': 'E',
+    'GAC': 'D',
+    'GAG': 'E',
+    'GAU': 'D',
+    'GCA': 'A',
+    'GCC': 'A',
+    'GCG': 'A',
+    'GCU': 'A',
+    'GGA': 'G',
+    'GGC': 'G',
+    'GGG': 'G',
+    'GGU': 'G',
+    'GUA': 'V',
+    'GUC': 'V',
+    'GUG': 'V',
+    'GUU': 'V',
+    'UAA': None,
+    'UAC': 'Y',
+    'UAG': None,
+    'UAU': 'Y',
+    'UCA': 'S',
+    'UCC': 'S',
+    'UCG': 'S',
+    'UCU': 'S',
+    'UGA': None,
+    'UGC': 'C',
+    'UGG': 'W',
+    'UGU': 'C',
+    'UUA': 'L',
+    'UUC': 'F',
+    'UUG': 'L',
+    'UUU': 'F',
+}
+
+def translate(sequence):
+    return ''.join(
+        [
+            amino
+            for amino in [
+                codon_table[sequence[_:_ + 3]]
+                for _ in
+                range(0, len(sequence), 3)
+                if len(sequence[_:_ + 3]) == 3
+            ]
+            if amino
+        ]
+    )
+
+if __name__ == '__main__':
+    sequence = open(sys.argv[1]).read()
+    print(translate(sequence))
